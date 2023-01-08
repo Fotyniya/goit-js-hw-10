fe74f68cc90df8368b6bd6f7e59f24d52c323310
Fotyniya <s.kozachenko@ekwo.dp.ua>
2023-01-08 18:06:27 +0200

    ready

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,12 @@
 function fetchCountries(name) {
     const BASE_URL = 'https://restcountries.com/v2';
-    const responce = fetch(`${BASE_URL}/name/${(name).trim()}?fields=name,capital,population,flags,languages`);
+    const responce = fetch(`${BASE_URL}/name/${(name)}?fields=name,capital,population,flags,languages`)
+    .then(resp => {
+        if (!resp.ok) {
+            throw new Error(resp.statusText);
+        }
+        return resp.json();
+    })
     return responce
 };
 
--- src/index.js
+++ src/index.js
@@ -12,41 +12,47 @@ const countryList = document.querySelector('.country-list');
 inputField.addEventListener('input', debounce(onSearchCountries, DEBOUNCE_DELAY))
 
 function onSearchCountries (name) {
-    if (inputField.value){
-        fetchCountries(inputField.value)
-        .then(resp => {
-            if (!resp.ok) {
-                throw new Error(Notify.failure("Oops, there is no country with that name"));
-            }
-            return resp.json();
-        })
+    onClearMarkup(); 
+    const value = inputField.value.trim();
+    if (!value){
+        return
+    } else {
+        fetchCountries(value)
         .then((data) => {
             if (data.length == 1) {
-                
-                let markupLang = '';
-                const markup = data.map (data => {
-                    let lang =[]
-                    for (let i = 0; i < data.languages.length; i += 1){
-                       lang.push(` ${(data.languages)[i].name}`);
-                       markupLang = `<p><span class = 'bold'>Languages:</span> ${lang}</p>`
-                    }
-                return `<h2 class = 'title'><img src="${data.flags.svg}" alt="flag" width = '30'>  ${data.name}</h2>
-                <p><span class = 'bold'>Capital:</span> ${data.capital}</p>
-                <p><span class = 'bold'>Population:</span> ${data.population}</p>
-                ${markupLang}`}).join();
-                
-                oneCountryFound.insertAdjacentHTML('afterbegin', markup)
-                
+                markupOneCountry(data)   
             } else if (data.length >= 2 & data.length <= 10){
-                const markup = data.map(data => {return `<h2 class = 'title'><img src="${data.flags.svg}" alt="flag" width = '30'>  ${data.name}</h2>`}).join('');
-               
-                countryList.insertAdjacentHTML('afterbegin', markup)
-            } 
-            else if(data.length > 10){
+                markupCountryList(data)
+            } else if(data.length > 10){
                 Notify.warning("Too many matches found. Please enter a more specific name.");
             } 
-        })   
-    } 
+        }).catch(error => Notify.warning("Oops, there is no country with that name"))   
+    }
+};
+
+function markupCountryList(data) {
+    const markup = data.map(data => 
+        {return `<h2 class = 'title'><img src="${data.flags.svg}" alt="flag" width = '30'>  ${data.name}</h2>`})
+        .join('');
+    countryList.insertAdjacentHTML('afterbegin', markup);   
+};
+
+function markupOneCountry(data) {
+    let markupLang = '';
+    const markup = data.map(data => {
+    let lang =[]
+    for (let i = 0; i < data.languages.length; i += 1){
+        lang.push(` ${(data.languages)[i].name}`);
+        markupLang = `<p><span class = 'bold'>Languages:</span> ${lang}</p>`
+    }
+    return `<h2 class = 'title'><img src="${data.flags.svg}" alt="flag" width = '30'>  ${data.name}</h2>
+    <p><span class = 'bold'>Capital:</span> ${data.capital}</p>
+    <p><span class = 'bold'>Population:</span> ${data.population}</p>
+    ${markupLang}`}).join();            
+    oneCountryFound.insertAdjacentHTML('afterbegin', markup)
+};
+
+function onClearMarkup() {
     oneCountryFound.innerHTML = "";
-    countryList.innerHTML = "";
+    countryList.innerHTML = "";    
 }
